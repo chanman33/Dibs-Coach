057c06ed701ced110b0847153f0bf2b50d654f39
Chandler <chandler.j.ward@gmail.com>
2025-04-11 11:53:51 -0600

    fix: svg fall back render homepage

--- next.config.js
+++ next.config.js
@@ -22,6 +22,8 @@ const nextConfig = {
   },
   
   images: {
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     remotePatterns: [{
       protocol: 'https',
       hostname: 'images.unsplash.com',
